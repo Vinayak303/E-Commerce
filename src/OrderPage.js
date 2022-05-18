5352b2fe6b8b73273e2164de21cee4271f6b0a5f
Vinayak303 <vinnurprabhu@gmail.com>
2022-05-18 08:33:52 +0530

    last

--- src/OrderPage.js
+++ src/OrderPage.js
@@ -57,7 +57,7 @@ function Contact() {
               className="appearance-none block w-full bg-white-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
               id="grid-last-name"
               type="text"
-              placeholder="Phone Number"
+              placeholder="+91"
               onChange={(event) => {
                 setLastname(event.target.value);
               }}
