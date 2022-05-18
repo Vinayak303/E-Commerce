784a17d02f8ab599c132926a52cf3a95786ea6bc
Vinayak303 <vinnurprabhu@gmail.com>
2022-05-18 08:44:32 +0530

    last

--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,6 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 function Header() {
   let navigate = useNavigate();
-  const user = localStorage.getItem("user");
   return (
     <div>
       <nav className="bg-white shadow">
@@ -11,29 +10,17 @@ function Header() {
 
           <div className="md:flex items-center">
             <div className="flex flex-col md:flex-row md:mx-6">
-              <div
-                onClick={() => {
-                  user ? navigate("/home") : navigate("/");
-                }}
-              >
+              <div onClick={navigate("/home")}>
                 <a className="my-1 text-sm text-gray-700 font-medium hover:text-indigo-500 md:mx-4 md:my-0">
                   Home
                 </a>
               </div>
-              <div
-                onClick={() => {
-                  user ? navigate("/contact") : navigate("/");
-                }}
-              >
+              <div onClick={navigate("/contact")}>
                 <a className="my-1 text-sm text-gray-700 font-medium hover:text-indigo-500 md:mx-4 md:my-0">
                   Contact Us
                 </a>
               </div>
-              <div
-                onClick={() => {
-                  user ? navigate("/about") : navigate("/");
-                }}
-              >
+              <div onClick={navigate("/about")}>
                 <a className="my-1 text-sm text-gray-700 font-medium hover:text-indigo-500 md:mx-4 md:my-0">
                   About
                 </a>
